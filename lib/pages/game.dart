c4af572c5fc76b95daa7c4bd9fc13744a628c15b
Tyler Kuhn <tyler@wargoats.com>
2020-04-07 18:24:09 -0700

    add some layout

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import '../data/words.dart';
@@ -12,30 +13,80 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  List<bool> redPips = List.from([true, true, true, false, false, false, false, false, false]);
+  List<bool> bluePips = List.from([false, false, false, false, false, false, false, false, false]);
 
   @override
   Widget build(BuildContext context) {
     List<String> wordList = getRandomWords(words(), 25);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.check),
+            tooltip: 'End Turn',
+            onPressed: () {
+              // openPage(context);
+            },
+          ),
+        ],
+      ),
+      body: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: redPips.map((filled) => _buildPlayerPip(context, Colors.red, Colors.red[100], filled)).toList(),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: bluePips.map((filled) => _buildPlayerPip(context, Colors.blue, Colors.blue[200], filled)).toList(),
+              ),
+            ),
+          ],
         ),
-        body: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(0),
-          crossAxisSpacing: 3,
-          mainAxisSpacing: 3,
-          crossAxisCount: 5,
-          children: wordList.map((word) => _buildTile(context, word)).toList(),
-        ));
+        Expanded(
+          child: GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(0),
+            crossAxisSpacing: 3,
+            mainAxisSpacing: 3,
+            crossAxisCount: 5,
+            children:
+                wordList.map((word) => _buildTile(context, word)).toList(),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: RaisedButton(
+            onPressed: () {},
+            child: const Text('End Turn'),
+            color: Colors.red[200],
+          ),
+        ),
+        Divider(),
+        SizedBox(
+          width: double.infinity,
+          child: RaisedButton(
+            onPressed: () {},
+            child: const Text('New Game'),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget _buildPlayerPip(BuildContext context, Color fillColor, Color baseColor, bool filled) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(2.0),
+        height: 10,
+        color: filled ? fillColor : baseColor
+      )
+    );
   }
 
   Widget _buildTile(BuildContext context, String word) {
