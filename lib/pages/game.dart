1bca34f5bac53047832f63bfe246b4e7e018c578
Tyler Kuhn <tyler@wargoats.com>
2020-04-07 23:41:12 -0700

    add initial game state

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -10,6 +10,13 @@ class GamePage extends StatefulWidget {
   GamePage({Key key, this.title}) : super(key: key);
 
   final String title;
+  final Player bomb = Player('bomb', Colors.grey, Colors.grey, 1);
+  final Set<String> turnStates = Set.from(['code_viewing', 'guessing']);
+
+  List<Tile> tiles = List();
+  List<Player> players = List();
+  String currentTurnState;
+  Player currentPlayer;
 
   @override
   _GamePageState createState() => _GamePageState();
@@ -19,42 +26,58 @@ class _GamePageState extends State<GamePage> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> wordList;
-    List<Tile> tiles;
-    List<Player> players;
-    Player bomb;
 
-    // enum TurnState {
-    //   codeviewing,
-    //   guessing,
-    // }
-
-    resetGame() {
+    void resetGame() {
       setState(() {
-        wordList = getRandomWords(words(), 25);
-        tiles = wordList.map((word) => Tile.fromWord(word)).toList();
-
-        players = [
+        widget.tiles = getRandomWords(words(), 25).map((word) => Tile.fromWord(word)).toList();
+        widget.players = [
           new Player('red', Colors.red[100], Colors.red, 9),
           new Player('blue', Colors.blue[100], Colors.blue, 8),
         ];
 
-        bomb = Player('bomb', Colors.grey, Colors.grey, 1);
-
-        List<Player> entities = players.toList();
-        entities.add(bomb);
+        List<Player> entities = widget.players.toList();
+        entities.add(widget.bomb);
         entities.forEach((player) {
-          while (tiles.where((tile) => tile.owner == player).length < player.tileCount) {
-            List<Tile> unassignedTiles = tiles.where((tile) => !tile.hasOwner()).toList();
+          while (widget.tiles.where((tile) => tile.owner == player).length < player.tileCount) {
+            List<Tile> unassignedTiles = widget.tiles.where((tile) => !tile.hasOwner()).toList();
             int n = Random().nextInt(unassignedTiles.length);
             Tile tile = unassignedTiles[n];
             tile.owner = player;
           }
         });
+
+        widget.currentTurnState = widget.turnStates.first;
+        widget.currentPlayer = widget.players.first;
       });
     }
 
-    resetGame();
+    if (widget.currentPlayer == null) {
+      resetGame();
+    }
+
+    String endTurnLabel() {
+      if (widget.currentTurnState == 'code_viewing') {
+        return "End Turn (Hide Codes)";
+      } else if (widget.currentTurnState == 'guessing') {
+        return "Begin Turn (View Codes)";//" 4 ${nextPlayer.name})";
+      }
+    }
+
+    void endTurn() {
+      // TODO: Clean up with lists, and next interations
+      setState(() {
+        if (widget.currentTurnState == 'code_viewing') {
+          widget.currentTurnState = 'guessing';
+          if (widget.currentPlayer == widget.players[0]) {
+            widget.currentPlayer = widget.players[1];
+          } else {
+            widget.currentPlayer = widget.players[0];
+          }
+        } else if (widget.currentTurnState == 'guessing') {
+          widget.currentTurnState = 'code_viewing';
+        }
+      });
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -64,21 +87,21 @@ class _GamePageState extends State<GamePage> {
             icon: const Icon(Icons.check),
             tooltip: 'End Turn',
             onPressed: () {
-              // end turn
+              endTurn();
             },
           ),
         ],
       ),
       body: Column(children: <Widget>[
         Row(
-          children: players.map((player) => _buildPlayerPips(context, player)).toList()
+          children: widget.players.map((player) => _buildPlayerPips(context, player)).toList()
         ),
         Expanded(
           child: Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topLeft,
-                colors: [players[0].baseColor, players[0].fillColor], // TODO
+                colors: [widget.players[0].baseColor, widget.players[0].fillColor], // TODO
               )
             ),
             child: GridView.count(
@@ -88,26 +111,22 @@ class _GamePageState extends State<GamePage> {
               mainAxisSpacing: 3,
               crossAxisCount: 5,
               children:
-                tiles.map((tile) => _buildTile(context, tile)).toList(),
+                widget.tiles.map((tile) => _buildTile(context, tile)).toList(),
             ),
           ),
         ),
         SizedBox(
           width: double.infinity,
           child: RaisedButton(
-            onPressed: () {
-              // End turn
-            },
-            child: const Text('End Turn'),
+            onPressed: () { endTurn(); },
+            child: Text(endTurnLabel()),
           ),
         ),
         Divider(),
         SizedBox(
           width: double.infinity,
           child: RaisedButton(
-            onPressed: () {
-              resetGame();
-            },
+            onPressed: () { resetGame(); },
             child: const Text('New Game'),
           ),
         ),
