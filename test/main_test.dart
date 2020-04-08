d83c30a2ae5c71b59d9d8c6996e91ccb2047d39d
Tyler Kuhn <tyler@wargoats.com>
2020-04-08 15:50:01 -0700

    add first widget test

---
+++ test/main_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:codefriendsflutter/main.dart';
+
+void main() {
+  testWidgets('displays a 2p game & hits all the buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.text('Pony Pasta'), findsOneWidget);
+
+    expect(find.text('New Game'), findsOneWidget);
+
+    // End code viewers turn...
+    await tester.tap(find.text("End Turn (Hide Codes)"));
+    await tester.pump();
+
+    expect(find.text('Begin Turn (View Codes 4 blue)'), findsOneWidget);
+
+    // End code guessers turn...
+    await tester.tap(find.byIcon(Icons.check));
+    await tester.pump();
+
+    expect(find.text('End Turn (Hide Codes)'), findsOneWidget);
+
+    // Start a new game...
+    await tester.tap(find.text("New Game"));
+    await tester.pump();
+
+    expect(find.text('End Turn (Hide Codes)'), findsOneWidget);
+  });
+}
