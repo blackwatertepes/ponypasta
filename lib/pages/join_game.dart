b71cab780adb0577aa744b58e6c003c5ab5feeb3
Tyler Kuhn <tyler@wargoats.com>
2020-04-09 16:01:50 -0700

    separate room creation and joining

---
+++ lib/pages/join_game.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import './game_online.dart';
+
+class JoinGamePage extends StatefulWidget {
+  JoinGamePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  String roomId;
+
+  @override
+  _JoinGamePageState createState() => _JoinGamePageState();
+}
+
+class _JoinGamePageState extends State<JoinGamePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Container(
+        color: Colors.grey[100],
+        child: Column(children: <Widget>[
+          Container(height: 50),
+          SizedBox(
+            width: 120,
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Enter room #:'),
+              keyboardType: TextInputType.number,
+              onChanged: (String number) {
+                widget.roomId = number;
+              }
+            )
+          ),
+          Container(height: 50),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: FlatButton(
+              color: Colors.grey[300],
+              splashColor: Colors.grey[200],
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GameOnlinePage(title: widget.title, roomId: widget.roomId)
+                  )
+                );
+              },
+              child: Text('Join Game'),
+            ),
+          ),
+        ]),
+      )
+    );
+  }
+}
--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,13 +1,14 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 import './game_online.dart';
 import './game_offline.dart';
+import './join_game.dart';
 
 class MenuPage extends StatefulWidget {
   MenuPage({Key key, this.title}) : super(key: key);
 
   final String title;
-  String roomId = '';
 
   @override
   _MenuPageState createState() => _MenuPageState();
@@ -18,56 +19,84 @@ class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
 
-    bool _hasRoomNumber() {
-      return widget.roomId.length >= 4;
+    String _generateRoomId() {
+      final Random random = new Random();
+      final int rand = random.nextInt(900000) + 100000;
+      return rand.toString();
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Center(child: Text(widget.title)),
       ),
-      body: Column(children: <Widget>[
-        SizedBox(
-          width: 120,
-          child: TextField(
-            decoration: InputDecoration(labelText: 'Enter room #:'),
-            keyboardType: TextInputType.number,
-            onChanged: (String number) {
-              widget.roomId = number;
-            }
+      body: Container(
+        color: Colors.grey[100],
+        child: Column(children: <Widget>[
+          Container(height: 50),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Center(
+              child: Text("Oddly similar to CodeNames, but totally not a rip-off."),
+            ),
           ),
-        ),
-        Divider(),
-        FractionallySizedBox(
-          widthFactor: 0.9,
-          child: OutlineButton(
-            onPressed: () {
-              if (_hasRoomNumber()) {
+          Container(height: 50),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: FlatButton(
+              color: Colors.grey[300],
+              splashColor: Colors.grey[200],
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => GameOnlinePage(title: widget.title, roomId: widget.roomId)
+                    builder: (context) => GameOnlinePage(title: widget.title, roomId: _generateRoomId())
                   )
                 );
-              }
-            },
-            child: Text('Multiplayer Game'),
+              },
+              child: Text('Create Multiplayer Game'),
+            ),
           ),
-        ),
-        Divider(),
-        FractionallySizedBox(
-          widthFactor: 0.9,
-          child: OutlineButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => GameOfflinePage(title: widget.title))
-              );
-            },
-            child: Text('Single Phone Game (Offline Mode)'),
+          Container(height: 20),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: FlatButton(
+              color: Colors.grey[300],
+              splashColor: Colors.grey[200],
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => JoinGamePage(title: widget.title)
+                  )
+                );
+              },
+              child: Text('Join Multiplayer Game'),
+            ),
           ),
-        ),
-      ]),
+          Container(height: 50),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Center(
+              child: Text("No Internet? Friend without a phone?"),
+            ),
+          ),
+          Container(height: 20),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: FlatButton(
+              color: Colors.grey[300],
+              splashColor: Colors.grey[200],
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GameOfflinePage(title: widget.title))
+                );
+              },
+              child: Text('Single Phone Game (Offline Mode)')
+            ),
+          ),
+        ]),
+      )
     );
   }
 }
