4eaa5749b9f16306455f615aef753ac398c16369
Tyler Kuhn <tyler@wargoats.com>
2020-04-14 00:31:07 -0700

    game no longer needs to store state

--- lib/components/game.dart
+++ lib/components/game.dart
@@ -8,23 +8,18 @@ import '../components/Pips.dart';
 import '../components/dialogs/game_over.dart';
 import '../services/database.dart';
 
-class GamePage extends StatefulWidget {
-  GamePage({Key key, this.title, this.roomId, this.isPlayer}) : super(key: key);
-
+class GamePage extends StatelessWidget {
   final String title;
   final String roomId;
   final String isPlayer;
   Game game;
 
-  @override
-  _GamePageState createState() => _GamePageState();
-}
+  GamePage({Key key, this.title, this.roomId, this.isPlayer}) : super(key: key);
 
-class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: DatabaseService.stream(widget.roomId),
+      stream: DatabaseService.stream(this.roomId),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
         if (snapshot.connectionState == ConnectionState.waiting) return new Text('Loading...');
@@ -36,11 +31,11 @@ class _GamePageState extends State<GamePage> {
 
   Widget buildGame(BuildContext context, Game game) {
     Player _isPlayer() {
-      return game.players.firstWhere((player) => player.name == widget.isPlayer);
+      return game.players.firstWhere((player) => player.name == this.isPlayer);
     }
 
     bool _isCurrent() {
-      return game.currentPlayer.name == widget.isPlayer;
+      return game.currentPlayer.name == this.isPlayer;
     }
 
     String _endTurnLabel() {
@@ -66,19 +61,17 @@ class _GamePageState extends State<GamePage> {
         tiles: game.tiles,
         onClickTile: (Tile tile) {
           if (_isCurrent()) {
-            setState(() {
-              final Player player = game.players.firstWhere((player) => player.name == tile.ownerName);
-              player.incScore();
-              tile.select();
-              if (tile.hasOwner() && tile.ownerName == "bomb") {
-                gameOver(context, "You've bown up!");
+            final Player player = game.players.firstWhere((player) => player.name == tile.ownerName);
+            player.incScore();
+            tile.select();
+            if (tile.hasOwner() && tile.ownerName == "bomb") {
+              gameOver(context, "You've bown up!");
+            }
+            if (tile.hasOwner() && tile.ownerName == game.currentPlayer.name) {
+              if (player.hasWon()) {
+                gameOver(context, "${tile.ownerName} has won!!!");
               }
-              if (tile.hasOwner() && tile.ownerName == game.currentPlayer.name) {
-                if (player.hasWon()) {
-                  gameOver(context, "${tile.ownerName} has won!!!");
-                }
-              }
-            });
+            }
           }
         }
       ),
